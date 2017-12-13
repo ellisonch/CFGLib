816fd4e70cc190de9c90fb9610c4e0e892e637ce
Chucky Ellison <cme@freefour.com>
2017-12-12 21:59:10 -0500

    sped up family hashing

--- CFGLib/Parsers/Earley/Family2.cs
+++ CFGLib/Parsers/Earley/Family2.cs
@@ -7,31 +7,36 @@ using System.Threading.Tasks;
 namespace CFGLib.Parsers.Earley {
 	internal class Family2 {
 		public List<SppfNode2> Children { get; }
+		private readonly int _cachedHash;
 
 		public Family2() {
 			Children = new List<SppfNode2> { };
+			_cachedHash = 0;
 		}
 		public Family2(SppfNode2 v) {
 			Children = new List<SppfNode2> { v };
+			_cachedHash = v.GetHashCode();
 		}
 		public Family2(SppfNode2 w, SppfNode2 v) {
 			Children = new List<SppfNode2> { w, v };
+			_cachedHash = unchecked((17 * 23 + w.GetHashCode()) * 23 + v.GetHashCode());
 		}
 
 		public override int GetHashCode() {
-			var first = Children.ElementAtOrDefault(0);
-			var second = Children.ElementAtOrDefault(1);
+			return _cachedHash;
+			//var first = Children.ElementAtOrDefault(0);
+			//var second = Children.ElementAtOrDefault(1);
 
-			//return new { first, second }.GetHashCode();
+			////return new { first, second }.GetHashCode();
 
-			// based on http://stackoverflow.com/a/263416/2877032
-			unchecked {
-				int hash = 17;
-				hash = hash * 23 + (first == null ? 0 : first.GetHashCode());
-				hash = hash * 23 + (second == null ? 0 : second.GetHashCode());
+			//// based on http://stackoverflow.com/a/263416/2877032
+			//unchecked {
+			//	int hash = 17;
+			//	hash = hash * 23 + (first == null ? 0 : first.GetHashCode());
+			//	hash = hash * 23 + (second == null ? 0 : second.GetHashCode());
 
-				return hash;
-			}
+			//	return hash;
+			//}
 		}
 
 		public override bool Equals(Object other) {
